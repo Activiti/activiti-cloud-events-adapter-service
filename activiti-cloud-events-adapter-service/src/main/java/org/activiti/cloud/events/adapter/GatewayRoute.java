3c20cbedac356d9825b4d631b50923d858f8124b
Cristina Sirbu <cristina.sirbu@ness.com>
2018-08-07 16:05:56 +0300

    Unmarshal list of events

--- activiti-cloud-events-adapter-service/src/main/java/org/activiti/cloud/events/adapter/GatewayRoute.java
+++ activiti-cloud-events-adapter-service/src/main/java/org/activiti/cloud/events/adapter/GatewayRoute.java
@@ -1,16 +1,22 @@
 package org.activiti.cloud.events.adapter;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.alfresco.event.databind.EventObjectMapperFactory;
 import org.alfresco.event.model.EventV1;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jackson.JacksonDataFormat;
-import org.apache.camel.spi.DataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Event gateway route builder.
@@ -26,37 +32,37 @@ public class GatewayRoute extends RouteBuilder {
     private final String camelFromRoute;
     private final String camelToRoute;
 
+    private ArrayList<LinkedHashMap> processResponses;
+
     @Autowired
     public GatewayRoute(String camelFromRoute, String camelToRoute) {
         this.camelFromRoute = camelFromRoute;
         this.camelToRoute = camelToRoute;
     }
 
-  /*  public void processResponseJSON(Exchange exchange) throws JsonParseException, JsonMappingException, IOException
+    private LinkedHashMap searchForEvent(String eventName)
+    {
+        LinkedHashMap eventFound = null;
+        for(LinkedHashMap event: processResponses) {
+            List<String> keys = new ArrayList<>(event.keySet());
+            for(String key:keys) {
+                if (event.get(key).equals(eventName)) {
+                    System.out.println("Event found: "+event.get(key));
+                    eventFound = event;
+                }
+            }
+        }
+        return eventFound;
+    }
+
+    private void getListOfEvents(Exchange exchange) throws JsonParseException, JsonMappingException, IOException
     {
         ObjectMapper mapper = new ObjectMapper();
 
-        List<ProcessStartedEvent> processResponses = mapper.readValue(exchange.getIn().getBody(String.class), new TypeReference<List<>>() { });
-        for(ProcessStartedEvent e:processResponses) {
-            System.out.println(e.applicationName);
-        }
-        //exchange.getIn().setBody(personSearchResponses);
+        processResponses = mapper.readValue(exchange.getIn().getBody(String.class), new TypeReference<List<?>>() { });
+        LinkedHashMap event = searchForEvent("TaskCreatedEvent");
+        EventV1 newEvent = new EventV1();
 
-    }*/
-
-    public static final ObjectMapper RAW_OBJECT_MAPPER = new ObjectMapper();
-    public static final ObjectMapper PUBLIC_OBJECT_MAPPER = EventObjectMapperFactory.createInstance();
-
-    @Bean
-    public DataFormat rawDataFormat()
-    {
-        return new JacksonDataFormat(RAW_OBJECT_MAPPER, Object.class);
-    }
-
-    @Bean
-    public DataFormat publicDataFormat()
-    {
-        return new JacksonDataFormat(PUBLIC_OBJECT_MAPPER, EventV1.class);
     }
 
     @Override
@@ -67,13 +73,12 @@ public class GatewayRoute extends RouteBuilder {
 
 
         from(camelFromRoute).log("Processing ${body}")
-//                .process(new Processor() {
-//                    @Override
-//                    public void process(Exchange exchange) throws Exception {
-//                        //processResponseJSON(exchange);
-//                }
-//                })
-
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        getListOfEvents(exchange);
+                }
+                })
                 .to(camelToRoute).log("finished");
 
     }
